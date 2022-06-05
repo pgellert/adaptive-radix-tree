f660a2d3577990b765ff07a77d29100054943fae
pgellert <pereszteginagy.gellert@gmail.com>
2022-06-05 18:39:08 +0100

    test: add delete operations to random test

--- tests/u64_art_map.rs
+++ tests/u64_art_map.rs
@@ -1,6 +1,8 @@
 extern crate adaptive_radix_tree;
 
 use adaptive_radix_tree::u64_art_map::*;
+use rand::distributions::Standard;
+use rand::prelude::Distribution;
 use rand::Rng;
 use std::collections::BTreeMap;
 
@@ -64,16 +66,43 @@ fn test_pop_first_and_pop_last_work() {
     assert_eq!(artmap.pop_last().unwrap().0, 400);
 }
 
+enum TestOperation {
+    Insert,
+    Delete,
+}
+
+impl Distribution<TestOperation> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> TestOperation {
+        match rng.gen_range(0..=3) {
+            // 25% Delete, 75% Insert
+            0 => TestOperation::Delete,
+            _ => TestOperation::Insert,
+        }
+    }
+}
+
 #[test]
-fn test_when_inserting_random_numbers_min_max_returns_same_as_btree() {
+fn test_when_executing_random_operations_min_max_returns_same_as_btree() {
     let mut artmap = U64ArtMap::<String>::new();
     let mut btree = BTreeMap::<u64, String>::new();
 
     let mut rng = rand::thread_rng();
     for _ in 0..10000 {
-        let random_key = rng.gen::<u64>();
-        artmap.insert(random_key, random_key.to_string());
-        btree.insert(random_key, random_key.to_string());
+        let random_op = rng.gen::<TestOperation>();
+        match random_op {
+            TestOperation::Insert => {
+                let random_key = rng.gen::<u64>();
+
+                artmap.insert(random_key, random_key.to_string());
+                btree.insert(random_key, random_key.to_string());
+            }
+            TestOperation::Delete => {
+                let random_key = rng.gen::<u64>();
+
+                artmap.delete(random_key);
+                btree.remove(&random_key);
+            }
+        }
 
         assert_eq!(artmap.minimum().unwrap().0, *btree.iter().next().unwrap().0);
         assert_eq!(artmap.maximum().unwrap().0, *btree.iter().last().unwrap().0);
